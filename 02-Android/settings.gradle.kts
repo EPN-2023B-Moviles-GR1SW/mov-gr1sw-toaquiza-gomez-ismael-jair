2640be5b5173b8c753f4b624bbf8a608f1e401b8
Ismael <ismaeltojair@gmail.com>
2023-11-08 15:48:16 -0500

    commit Inicial

---
+++ 02-Android/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "b2023-gr1sw-ijtg"
+include(":app")
+ 
